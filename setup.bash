6d7b6709a13c7ed6395e3fa276795583bf51a47b
Jonas <jonas.moritz.mueller@campus.tu-berlin.de>
2025-09-09 01:23:19 +0200

    deleting only ghost folders not all empty folders

--- setup.bash
+++ setup.bash
@@ -36,7 +36,8 @@ find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete
 # then delete git untracked folders and empty folders
 git clean -fd>/dev/null
 
-find . -type d -empty -delete
+# delete ghost folders with specific name
+find . -name "Aufgabe*" -type d -empty -delete
 
 echo "✅ post-checkout cleanup finished. Working tree is clean."
 EOF
