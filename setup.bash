56d235fcb8ae83c4b1f66614d32fba0f71de421e
Jonas <jonas.moritz.mueller@campus.tu-berlin.de>
2025-09-09 01:19:13 +0200

    even adding deleting emopty folder back

--- setup.bash
+++ setup.bash
@@ -36,7 +36,7 @@ find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete
 # then delete git untracked folders and empty folders
 git clean -fd>/dev/null
 
-# find . -type d -empty -delete
+find . -type d -empty -delete
 
 echo "✅ post-checkout cleanup finished. Working tree is clean."
 EOF
